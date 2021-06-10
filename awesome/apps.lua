b552c31897978aa21e0307c7e9ebac7f245fb2a7
Daniel Dides <senorequeso@gmail.com>
2021-06-09 22:42:38 -0500

    awesome: move autostart back to xinit

--- awesome/apps.lua
+++ awesome/apps.lua
@@ -14,28 +14,4 @@ apps.default = {
    filebrowser = "thunar"
 }
 
--- List of apps to start once on start-up
-local run_on_start_up = {
-   "picom --config /home/daniel.config/picom.conf",
-   "unclutter",
-   "nm-applet",
-   "blueman-applet",
-   "xfce4-power-manager",
-   "redshift-gtk",
-   "udiskie",
-   "thunar --daemon"
-}
-
--- Run all the apps listed in run_on_start_up
-function apps.autostart()
-   for _, app in ipairs(run_on_start_up) do
-      local findme = app
-      local firstspace = app:find(" ")
-      if firstspace then
-         findme = app:sub(0, firstspace - 1)
-      end
-         awful.spawn.with_shell(string.format("pgrep -u $USER -x %s > /dev/null || (%s)", findme, app), false)
-   end
-end
-
 return apps
